e70a6dbd1e9d4d6744c28007dbc937460729d34f
Emily Manders <manders.emily@gmail.com>
2015-12-17 12:16:18 -0800

    [shipments] Added shipments page

--- goingpostal-app/urls.py
+++ goingpostal-app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^shipments/$', views.shipments, name='shipments'),
 ]
--- goingpostal-app/views.py
+++ goingpostal-app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'goingpostal-app/index.html')
+
+
+def shipments(request):
+    return render(request, 'goingpostal-app/shipments.html')
