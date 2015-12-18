c20aedae59f1ac14a356b3a55b93042db48c2f7c
Emily Manders <manders.emily@gmail.com>
2015-12-17 16:00:17 -0800

    [models] Added Shipment and Location models, first db migration

---
+++ goingpostal-app/migrations/0001_initial.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('city', models.CharField(max_length=64, blank=True)),
+                ('state', models.CharField(max_length=64, blank=True)),
+                ('country', models.CharField(max_length=64, blank=True)),
+                ('latitude', models.CharField(max_length=64, blank=True)),
+                ('longitude', models.CharField(max_length=64, blank=True)),
+                ('timestamp', models.DateTimeField()),
+                ('status_description', models.CharField(max_length=64)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Shipment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('tracking_no', models.CharField(max_length=30)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='location',
+            name='shipment',
+            field=models.ForeignKey(to='goingpostal-app.Shipment'),
+        ),
+    ]
--- goingpostal-app/models.py
+++ goingpostal-app/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Shipment(models.Model):
+    tracking_no = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tracking_no
+
+    def track(self):
+        pass
+
+    def get_last_activity(self):
+        pass
+
+
+class Location(models.Model):
+    shipment = models.ForeignKey(Shipment, on_delete=models.CASCADE)
+    city = models.CharField(max_length=64, blank=True)
+    state = models.CharField(max_length=64, blank=True)
+    country = models.CharField(max_length=64, blank=True)
+    latitude = models.CharField(max_length=64, blank=True)
+    longitude = models.CharField(max_length=64, blank=True)
+    timestamp = models.DateTimeField()
+    status_description = models.CharField(max_length=64)
