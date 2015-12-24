6b87d64ee9b0ce2c63a874740e8af739e8775c75
Emily Manders <manders.emily@gmail.com>
2015-12-23 17:39:07 -0800

    [map] Disable zooming when scrolling over the map

--- goingpostal_app/static/goingpostal_app/js/mapper.js
+++ goingpostal_app/static/goingpostal_app/js/mapper.js
@@ -5,6 +5,7 @@ var Mapper = (function() {
   function init() {
     GP.map = new google.maps.Map(document.getElementById('map-canvas'), {
       center: {lat: 36.3955, lng: -97.8783},
+      scrollwheel: false,
       zoom: 5
     });
   }
