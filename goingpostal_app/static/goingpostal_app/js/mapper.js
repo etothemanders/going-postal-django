26d8b084e05e8ccf27eae123e5d535f26b444024
Emily Manders <manders.emily@gmail.com>
2015-12-27 13:15:08 -0800

    [mapper] Only load geojson if there are features to draw

--- goingpostal_app/static/goingpostal_app/js/mapper.js
+++ goingpostal_app/static/goingpostal_app/js/mapper.js
@@ -2,14 +2,25 @@ var GP = GP || {};
 
 var Mapper = (function() {
 
-  function init() {
+  function addMap() {
     GP.map = new google.maps.Map(document.getElementById("map-canvas"), {
       center: {lat: 36.3955, lng: -97.8783},
       scrollwheel: false,
       zoom: 5
     });
+  }
 
-    GP.map.data.loadGeoJson("/load_geojson");
+  function drawFeatures() {
+    var $shipmentsTable = $(".table");
+    
+    if ($shipmentsTable.length > 0) {
+      GP.map.data.loadGeoJson("/load_geojson/");
+    }
+  }
+
+  function init() {
+    addMap();
+    drawFeatures();
   }
 
   return {
