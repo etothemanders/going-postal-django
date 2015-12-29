e38b08cf6aff3ab1c2e445406e5c1a3579e7b323
Emily Manders <manders.emily@gmail.com>
2015-12-28 17:19:51 -0800

    [settings] Goingpostal and third party apps must be listed before django apps

--- goingpostal/settings.py
+++ goingpostal/settings.py
@@ -33,14 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'goingpostal_app',
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'goingpostal_app',
-    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
